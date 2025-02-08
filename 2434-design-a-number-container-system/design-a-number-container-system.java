521c7bce6885c5a989a29dcbcf7bf313353a79fd
Tulsi_Shivpure <125723227+TulsiShivpure@users.noreply.github.com>
2025-02-08 09:38:10 +0530

    Time: 72 ms (69.86%) | Memory: 97.8 MB (86.99%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -0,0 +1,44 @@
+import java.util.NoSuchElementException;
+
+class NumberContainers {
+    Map<Integer, Integer> map;
+    Map<Integer, SortedSet<Integer>> valIdx;
+    public NumberContainers() {
+        map = new HashMap<>();
+        valIdx = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        if (!valIdx.containsKey(number)) {
+            valIdx.put(number, new TreeSet<>());
+        }
+
+        if (map.containsKey(index)) {
+            if(map.get(index)==number) return;
+            int prevVal = map.get(index);
+            valIdx.get(prevVal).remove(index); 
+        }
+
+        valIdx.get(number).add(index);
+        map.put(index, number);
+    }
+
+    public int find(int number) {
+        if (valIdx.containsKey(number)) {
+            try {
+                return valIdx.get(number).first();
+            } catch (NoSuchElementException e) {
+                return -1;
+            }
+        }
+        // If the number is not found, return -1
+        return -1;
+    }
+}
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
