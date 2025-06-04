6cdc9fdae4bac8911539a15c910d84beb15d6030
Tulsi_Shivpure <125723227+TulsiShivpure@users.noreply.github.com>
2025-06-04 22:37:52 +0530

    Time: 1 ms (99.92%) | Memory: 46.3 MB (46.98%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,24 +1,32 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        // int n=nums.length;
+        // int pos=0;
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // Arrays.sort(nums);
+        // if(nums[0]!=nums[1]){
+        //     return nums[0];
+        // }
+        // for(int i=1;i<n;i++){
+        //     if((i+1<n)&&(nums[i]!=nums[i-1]) && (nums[i]!=nums[i+1])){
+        //         return nums[i];
+        //     }
+
+
+        // }
+        // if(nums[n-2]!=nums[n-1]){
+        //     return nums[n-1];
+        // }
+        // return -1;
+
         int n=nums.length;
-        int pos=0;
-        if(n==1){
-            return nums[0];
-        }
-        Arrays.sort(nums);
-        if(nums[0]!=nums[1]){
-            return nums[0];
-        }
-        for(int i=1;i<n;i++){
-            if((i+1<n)&&(nums[i]!=nums[i-1]) && (nums[i]!=nums[i+1])){
-                return nums[i];
-            }
-
+        int result=0;
+        for(int i=0;i<n;i++){
+            result=result^nums[i];
 
         }
-        if(nums[n-2]!=nums[n-1]){
-            return nums[n-1];
-        }
-        return -1;
+        return result;
     }
 }
