fe056cff1742f8a3036507189a1f396755c1923b
Tulsi_Shivpure <125723227+TulsiShivpure@users.noreply.github.com>
2025-06-04 22:35:08 +0530

    Time: 8 ms (26.00%) | Memory: 46.3 MB (46.98%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=nums.length;
+        int pos=0;
+        if(n==1){
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        for(int i=1;i<n;i++){
+            if((i+1<n)&&(nums[i]!=nums[i-1]) && (nums[i]!=nums[i+1])){
+                return nums[i];
+            }
+
+
+        }
+        if(nums[n-2]!=nums[n-1]){
+            return nums[n-1];
+        }
+        return -1;
+    }
+}
